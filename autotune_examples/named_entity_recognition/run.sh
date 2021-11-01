bd89220ba89d7f4b713ba9a3d9ea769cb46e9fea
sizhky <1992chinna@gmail.com>
2021-11-01 21:53:52 +0530

    named-entity-recognition

---
+++ autotune_examples/named_entity_recognition/run.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+export AUTOTUNE_CONFIG=/home/yyr/code/auto-nlp-apis/autotune_examples/named_entity_recognition/config.ini
+python /home/yyr/code/auto-nlp-apis/autotune/named_entity_recognition/main.py
